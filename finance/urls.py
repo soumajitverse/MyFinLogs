504a06bded880935fa8ffe929fb08106dd074120
Soumajit Majumder <majumdersoumait09@gmail.com>
2025-07-04 15:53:23 +0530

    Initial commit with project files

---
+++ finance/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from finance.views import RegisterView, DashboardView, TransactionCreateView, TransactionListView, GoalCreateView, export_transactions, GoalDeleteView
+urlpatterns = [
+  path('register/', RegisterView.as_view(), name="register"),    
+  path('', DashboardView.as_view(), name="dashboard"),
+  path('transaction/add/', TransactionCreateView.as_view(), name='transaction_add'),
+  path('transactions/', TransactionListView.as_view(), name='transaction_list'), 
+  path('goal/add/', GoalCreateView.as_view(), name='goal_add'),
+  path('generate-report/', export_transactions, name='export_transactions'),
+  path('goals/<int:pk>/delete/', GoalDeleteView.as_view(), name='goal_delete'),
+]
